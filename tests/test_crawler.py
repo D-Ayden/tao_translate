cbbf23a9a33a49ebe1777b793fdd8d38a2e02f0b
D-zlich <2361018131@qq.com>
2021-01-02 17:50:13 +0800

    master

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -3,3 +3,15 @@ from ..crawler import TRANSLATE_URL, yd_translate
 
 def test_yd_translate():
     pass
+
+
+def test_matching_result():
+    pass
+
+
+def test_update_sugar():
+    pass
+
+
+def test_get_sugar():
+    pass
